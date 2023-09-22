49f82c080ca27b0497112df5b6a614fcfb1aabef
artemsmartaxmt880 <artemsmartaxmt880@mail.ru>
2023-09-22 21:29:46 +0300

    button  star work

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+const btn = document.querySelector('.test__btn');
+const q1 = document.getElementsByName("q1");
 
+// q1.forEach(radio => {
+//     if (radio.checked) {
+//     }
+// });
+btn.addEventListener('click',pos);
+function pos(){
+    for (let i = 0; i < q1.length; i++) {
+        if (q1[i].checked) {
+            console.log(q1[i].value);
+            break;
+        }
+    }
+}
 
